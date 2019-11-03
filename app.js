ea676e765c45a61bf8786f4148891c3d6110095f
PatinyaNudklin <patinyanudklin@gmail.com>
2019-11-03 11:25:03 +0700

    Add pug view engine, SCG controller, and mainpage views

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const scgController = require('./controllers/SCG')
 const app = express()
 
+app.set('views', './views')
 app.set('view engine', 'pug')
 
-app.get('/',  scgController.findingXYZ)
+app.get('/',  scgController.findingRestaurantsInBangsue)
 
 app.listen(3000)
--- controllers/SCG.js
+++ controllers/SCG.js
@@ -1,7 +1,25 @@
+const request = require('request')
+const { StringDecoder } = require('string_decoder');
+const decoder = new StringDecoder('utf8');
+
 exports.findingXYZ = function(req, res){
-	res.send("hello")
+	res.render("mainpage")
 }
 
 exports.findingRestaurantsInBangsue = function(req, res){
+	//const url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=%E0%B8%AD%E0%B8%B2%E0%B8%AB%E0%B8%B2%E0%B8%A3&inputtype=textquery&fields=formatted_address,geometry,icon,name,types&locationbias=rectangle:13.797062,100.505889|13.849573,100.545197&key=AIzaSyDr6n-dNDoMM8PavoXgwKxBofT8Rez0Z7A'
+	// Nearby Search
+	const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=13.825820,100.523141&radius=3500&type=restaurant&key=AIzaSyDr6n-dNDoMM8PavoXgwKxBofT8Rez0Z7A'
 	
+	request(url, function(error, response, body){
+		if(!error&& response.statusCode == 200){
+			let jsonResult = JSON.parse(body);
+			let jsonText = JSON.stringify(jsonResult, null, 4)
+			//console.log(jsonResult);
+			//res.render("mainpage", {body})
+			res.render('mainpage', {jsonText})
+		}
+	})
+	//console.log("ddd"+jsonResult)
+	//request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=13.825820,100.523141&radius=3500&type=restaurant&key=AIzaSyDr6n-dNDoMM8PavoXgwKxBofT8Rez0Z7A')
 }
